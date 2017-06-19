44d97379e4bd63eeddc9a4de623be012285023d4
Jordan Smith <cjordansmith@gmail.com>
2017-06-19 02:26:56 -0500

    contact form javascript, changed base colors

--- js/code.js
+++ js/code.js
@@ -1,21 +1,4 @@
-// var granimInstance = new Granim({
-//     element: '#logo-canvas',
-//     direction: 'left-right',
-//     opacity: [1, 1],
-//     states : {
-//         "default-state": {
-//             gradients: [
-//                 ['#EB3349', '#F45C43'],
-//                 ['#FF8008', '#FFC837'],
-//                 ['#4CB8C4', '#3CD3AD'],
-//                 ['#24C6DC', '#514A9D'],
-//                 ['#FF512F', '#DD2476'],
-//                 ['#DA22FF', '#9733EE']
-//             ],
-//             transitionSpeed: 2000
-//         }
-//     }
-// });
+
 
 var granimInstance = new Granim({
     element: '#canvas-basic',
@@ -37,3 +20,20 @@ var granimInstance = new Granim({
         }
     }
 });
+
+// function showContact() {
+// 	var x = document.getElementById('contact-frame');
+// 	x.style.display = 'block';
+// }
+// function hideContact() {
+// 	var x = document.getElementById('contact-frame');
+// 	x.style.display = 'none';
+// }
+function showContact() {
+    var x = document.getElementById('contact-frame');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
