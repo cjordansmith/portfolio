18619a5d0e1a5152ddf400842a23ef71a1bae78a
Jordan Smith <cjordansmith@gmail.com>
2017-06-20 02:55:26 -0500

    more styling edits

--- js/code.js
+++ js/code.js
@@ -31,9 +31,13 @@ var granimInstance = new Granim({
 // }
 function showContact() {
     var x = document.getElementById('contact-frame');
+		var y = document.getElementById('cover-text');
+
     if (x.style.display === 'block') {
         x.style.display = 'none';
+				y.style.display = 'block';
     } else {
         x.style.display = 'block';
+				y.style.display = 'none';
     }
 }
